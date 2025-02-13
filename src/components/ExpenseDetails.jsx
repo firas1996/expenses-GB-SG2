39768cb12ea0b2abeb10d2f6e4d4f3c0d211a07a
firas hamila <firas.hm.1996@gmail.com>
2025-02-13 15:59:24 +0100

    a

---
+++ src/components/ExpenseDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseDetails = () => {
+  return (
+    <div>
+      <div>date</div>
+      <div>
+        <h2>title</h2>
+        <div>price</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseDetails;
