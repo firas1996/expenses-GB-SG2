d13f4654b3f46754476b8c1c6d079379349b644e
firas hamila <firas.hm.1996@gmail.com>
2025-02-13 16:54:48 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import ExpenseDetails from "./components/ExpenseDetails";
 
 function App() {
+  const expensesData = [
+    {
+      id: 1,
+      title: "TV",
+      price: 500,
+      date: new Date("2025-02-13"),
+    },
+    {
+      id: 2,
+      title: "PC",
+      price: 700,
+      date: new Date("2024-08-13"),
+    },
+    {
+      id: 3,
+      title: "Car",
+      price: 200,
+      date: new Date("2023-10-13"),
+    },
+    {
+      id: 4,
+      title: "Education",
+      price: 800,
+      date: new Date("2025-04-13"),
+    },
+  ];
   return (
     <div>
-      <ExpenseDetails />
+      {expensesData.map((expense) => {
+        return (
+          <ExpenseDetails
+            key={expense.id}
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+          />
+        );
+      })}
     </div>
   );
 }
