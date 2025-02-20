5032a2d2401e5b804ef595590f4f99d00e835962
firas hamila <firas.hm.1996@gmail.com>
2025-02-20 14:31:16 +0100

    a

--- src/components/ExpenseDetails.jsx
+++ src/components/ExpenseDetails.jsx
@@ -1,12 +1,13 @@
 import "./ExpenseDetails.css";
 
-const ExpenseDetails = () => {
+const ExpenseDetails = ({ title, price }) => {
+  // const { title, price } = props;
   return (
     <div className="expense-item">
       <div className="expense-date">date</div>
       <div className="expense-item__description">
-        <h2>title</h2>
-        <div className="expense-item__price">price</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
   );
