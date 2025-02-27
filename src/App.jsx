dc1315ab0b7db510c41c8dd9b3e4e20657d5228b
firas hamila <firas.hm.1996@gmail.com>
2025-02-27 15:21:13 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,12 @@ function App() {
       price: 800,
       date: new Date("2025-04-13"),
     },
+    {
+      id: 5,
+      title: "Education",
+      price: 800,
+      date: new Date("2027-04-13"),
+    },
   ];
   return (
     <div>
--- src/components/ExpensesContainer.jsx
+++ src/components/ExpensesContainer.jsx
@@ -3,6 +3,11 @@ import "./ExpensesContainer.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const ExpensesContainer = ({ expensesData }) => {
+  const years = [
+    "All",
+    ...new Set(expensesData.map((el) => el.date.getFullYear()).sort()),
+  ];
+  console.log(years);
   return (
     <div className="expenses">
       <ExpensesFilter />
